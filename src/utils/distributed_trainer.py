4ad1634e054315191e334f541704a31eaddd530b
Marco Del Tutto <marco.deltutto@gmail.com>
2019-06-28 14:26:41 -0400

    Remoning key argument read_from_all_ranks as no longer in larcv

--- src/utils/distributed_trainer.py
+++ src/utils/distributed_trainer.py
@@ -127,7 +127,7 @@ class distributed_trainer(trainercore):
             os.environ['CUDA_VISIBLE_DEVICES'] = str(hvd.local_rank())
             
 
-        self._larcv_interface = larcv_interface(root=root_rank, read_from_all_ranks=True, read_option=FLAGS.READ_OPTION, local_rank=hvd.local_rank(), local_size=hvd.local_size())
+        self._larcv_interface = larcv_interface(root=root_rank, read_option=FLAGS.READ_OPTION, local_rank=hvd.local_rank(), local_size=hvd.local_size())
         self._iteration       = 0
         self._rank            = hvd.rank()
         self._local_rank      = hvd.local_rank()
