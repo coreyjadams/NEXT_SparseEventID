1cf670ba18bbe4e6123f90d5d8f058c4ff8818c4
Marco Del Tutto <marco.deltutto@gmail.com>
2019-08-26 14:08:35 -0400

    For inference

--- src/utils/trainercore.py
+++ src/utils/trainercore.py
@@ -86,6 +86,11 @@ class trainercore(object):
         # Assign the keywords here:
         FLAGS.KEYWORD_LABEL = 'label'
 
+        FLAGS.MPI_IO = True
+
+        #if FLAGS.MPI_IO:
+        #    self._larcv_interface.prepare_manager('primary', io_config, FLAGS.MINIBATCH_SIZE, data_keys)
+        #else:
         self._larcv_interface.prepare_manager('primary', io_config, FLAGS.MINIBATCH_SIZE, data_keys)
 
         print ('done prepare_manager')
@@ -129,6 +134,8 @@ class trainercore(object):
 
         if FLAGS.OUTPUT_FILE is not None:
             if not FLAGS.TRAINING:
+                self._outfile = open(FLAGS.OUTPUT_FILE, 'w')
+                '''
                 config = io_templates.output_io(input_file=FLAGS.FILE, output_file=FLAGS.OUTPUT_FILE)
                 
                 out_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
@@ -139,7 +146,7 @@ class trainercore(object):
                 self._cleanup.append(out_file)
 
                 self._larcv_interface.prepare_writer(out_file.name, FLAGS.OUTPUT_FILE)
-
+                '''
 
 
     def init_network(self):
@@ -737,9 +744,7 @@ class trainercore(object):
 
                 return metrics
 
-    def ana_step(self, iteration=None):
-
-        print ('ana_step starts')
+    def inference_step(self, iteration=None):
         # First, validation only occurs on training:
         if FLAGS.TRAINING: return
 
@@ -752,7 +757,46 @@ class trainercore(object):
         # Fetch the next batch of data with larcv
         minibatch_data = self.fetch_next_batch(metadata=True)
 
-        print ('minibatch_data fetched')
+        # Convert the input data to torch tensors
+        minibatch_data = self.to_torch(minibatch_data)
+
+        # Run a forward pass of the model on the input image:
+        with torch.no_grad():
+            logits = self._net(minibatch_data['image'])
+
+        values, predict = torch.max(logits, dim=1)
+
+        prediction_label = predict.data.cpu().numpy()[0]
+        total_energy = numpy.sum(minibatch_data['image'][1].data.cpu().numpy())
+        self._outfile.write(str(iteration) + ',' + str(prediction_label) + ',' + str(total_energy) + '\n')
+        
+        '''
+        larcv_particle = larcv.EventParticle.to_particle(self._io_mgr.get_data("particle", "label"))
+        particle = larcv.Particle()
+        particle.energy_init(0.)
+        particle.pdg_code(int(predict.data))
+        larcv_particle.emplace_back(particle)
+
+        event_sparse3d = larcv.EventSparseTensor3D.to_sparse_tensor(self._io_mgr.get_data("sparse3d", "voxels"))
+        st = larcv.SparseTensor3D()
+        st.meta(next_new_meta)
+        '''
+
+
+    def ana_step(self, iteration=None):
+
+        # First, validation only occurs on training:
+        if FLAGS.TRAINING: return
+
+        # perform a validation step
+
+        # Set network to eval mode
+        self._net.eval()
+        # self._net.train()
+
+        # Fetch the next batch of data with larcv
+        minibatch_data = self.fetch_next_batch(metadata=True)
+
 
         # Convert the input data to torch tensors
         minibatch_data = self.to_torch(minibatch_data)
@@ -761,16 +805,7 @@ class trainercore(object):
         with torch.no_grad():
             logits = self._net(minibatch_data['image'])
 
-        print ('forward pass run')
 
-        values, predict = torch.max(logits, dim=1)
-
-        print (predict)
-        print (torch.eq(predict,1))
-        exit()
-        
-
-        '''
         if FLAGS.LABEL_MODE == 'all':
             softmax = torch.nn.Softmax(dim=-1)(logits)
         else:
@@ -811,7 +846,6 @@ class trainercore(object):
             # self.summary(metrics, saver="test")
 
             return metrics
-   '''
 
     def stop(self):
         # Mostly, this is just turning off the io:
@@ -851,8 +885,10 @@ class trainercore(object):
                 self.val_step()
                 self.train_step()
                 self.checkpoint()
-            else:
+            elif FLAGS.INFERENCE:
                 print ('iteration', i)
+                self.inference_step(i)
+            else:
                 self.ana_step(i)
 
 
